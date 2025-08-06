aea62b08c16f11ac553e12ee80f11b655b353eb0
Divinegift007 <divinegiftayeni@gmail.com>
2025-08-06 11:34:48 +0100

    refactor(llm_config): changed imports from langchain.llms to from langchain_community.llms

--- xiara/core/llm_config.py
+++ xiara/core/llm_config.py
@@ -1,4 +1,4 @@
-from langchain.llms import LlamaCpp
+from langchain_community.llms import LlamaCpp
 
 llm = LlamaCpp(
     model_path="C:/Users/MOSES/Desktop/PASAR Agentic AI/llama-2-7b.Q4_K_M.gguf",  # install "llama-2-7b.Q4_K_M.gguf" and set the path 
