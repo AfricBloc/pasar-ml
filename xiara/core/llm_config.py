dfd8fd42aedd2016fe14638479b23d14eb88342b
Divinegift007 <divinegiftayeni@gmail.com>
2025-08-08 12:10:35 +0100

    refactor(llm_config.py): switch from llama2-7b to tinyllama 1.1b_Q4_K_M model

--- xiara/core/llm_config.py
+++ xiara/core/llm_config.py
@@ -1,10 +1,14 @@
 from langchain_community.llms import LlamaCpp
 
 llm = LlamaCpp(
-    model_path="C:/Users/MOSES/Desktop/PASAR Agentic AI/llama-2-7b.Q4_K_M.gguf",  # install "llama-2-7b.Q4_K_M.gguf" and set the path 
+    model_path="C:/Users/MOSES/Desktop/PASAR Agentic AI/tinyllama-1.1b-chat-v1.0.Q4_K_M.gguf",  #  tinyllama alternative and set the path 
     n_ctx=2048,
     temperature=0.7,
     top_p=0.95,
     verbose=True,
     n_threads=4  
 )
+
+
+# model_path="C:/Users/MOSES/Desktop/PASAR Agentic AI/llama-2-7b.Q4_K_M.gguf",  # install "llama-2-7b.Q4_K_M.gguf" and set the path 
+   
